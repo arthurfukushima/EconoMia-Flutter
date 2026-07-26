faf9796857e4853ddff6d92da6b657225b336a8d
Hugo Mac <hugomatsu@gmail.com>
2026-07-26 20:08:36 -0300

    change: mis logo and build

--- lib/core/build_info.dart
+++ lib/core/build_info.dart
@@ -2,5 +2,5 @@
 // Updated by the build script before each release build.
 // ignore_for_file: constant_identifier_names
 
-const String kBuildVersion = '1.0.0+3';
-const String kBuildTime = '2026-07-26T19:57:54Z';
+const String kBuildVersion = '1.0.0+4';
+const String kBuildTime = '2026-07-26T22:52:13Z';
